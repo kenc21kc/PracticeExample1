b723980200fc0f49c8d53221f1cbbb11d72a4796
ken <chuak@sheridancollege.ca>
2022-09-24 23:59:54 -0400

    First Version

---
+++ src/main/java/com/example/PracticeExample1/PracticeExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.PracticeExample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeExample1Application.class, args);
+	}
+
+}
